c5a02330ca8f9a4dbf8d809916a61b8bffb33147
pdbautista0 <patdi.bautista@gmail.com>
2023-05-31 22:11:25 +0800

    Added Subjects Page
    Created Subjects Page
    Created AllSubjectsPage

---
+++ src/pages/Subjects/All/AllSubjects.tsx
@@ -0,0 +1,200 @@
+import React, { createRef, useState } from "react";
+//types & interface
+import { Subject } from "../utils/interfaces";
+
+//tyle-components
+import styled from "styled-components";
+
+//Mui Imports
+import { Pagination } from "@mui/material";
+import SubjectsTable from "../../../compenents/SubjectsTable";
+
+const Container = styled.div`
+  width: 100%;
+  height: 1000px;
+  border-radius: 10px;
+  box-shadow: 0 0 7px lightgray;
+  background-color: white;
+  padding: 40px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  position: relative;
+`;
+const Title = styled.h1`
+  width: 100%;
+  margin-bottom: 40px;
+`;
+
+const SearchContainer = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: 1fr 0.75fr 0.75fr 0.5fr;
+  grid-template-rows: 75px;
+  gap: 30px;
+  margin-bottom: 50px;
+`;
+
+const InputName = styled.input`
+  background-color: #f0f1f3;
+  border: none;
+  border-radius: 10px;
+  color: gray;
+  padding: 10px;
+  font-size: 1.25rem;
+`;
+const Selection = styled.select`
+  /* -webkit-appearance: none;
+  -moz-appearance: none; */
+  background-color: #f0f1f3;
+  border: none;
+  border-radius: 10px;
+  color: gray;
+  padding: 10px;
+  padding-right: 20px;
+  font-size: 1.25rem;
+
+  ::after {
+    content: "Hello";
+  }
+`;
+
+const SelectionItem = styled.option``;
+
+const ButtonSearch = styled.button`
+  font-size: 1.25rem;
+  color: white;
+  background-color: red;
+  border: none;
+  border-radius: 10px;
+
+  &:hover {
+    background-color: #d00000;
+  }
+`;
+
+const PagerContainer = styled.div`
+  height: 100px;
+  width: 100%;
+  margin-top: 10px;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const AllSubjects = () => {
+  const gradeLevelRef = createRef<HTMLSelectElement>();
+  const sectionRef = createRef<HTMLSelectElement>();
+  const [searchName, setSearchName] = useState("");
+  const pageCount = 1;
+  const currentPage = 1;
+  const data: Array<Subject> = [];
+
+  const handleOnRowsPerChange = (e) => {
+    console.log(e.targe.value);
+  };
+
+  const handlerSearch = async () => {
+    // if (searchName === "" && className === "") {
+    //   setData(studentList2);
+    // } else if (searchName === "" && className !== "") {
+    //   setData(
+    //     studentList2.filter((student) => {
+    //       return className === student.std_c_name;
+    //     })
+    //   );
+    // } else if (searchName !== "" && className === "") {
+    //   setData(
+    //     studentList2.filter((student) => {
+    //       return student.std_name.match(`.*${searchName}.*`);
+    //     })
+    //   );
+    // } else {
+    //   setData(
+    //     data.filter((student) => {
+    //       return (
+    //         studentList2.std_name.match(`.*${searchName}.*`) &&
+    //         className === student.std_c_name
+    //       );
+    //     })
+    //   );
+    // }
+    // setSearchName("");
+    // setClassName("");
+  };
+
+  const handlePressEnter = (event) => {
+    if (event.which === 13) {
+      handlerSearch();
+    }
+  };
+
+  const handlerSearchName = (e) => {
+    setSearchName(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handleClassName = (e) => {
+    // setClassName(e.target.value);
+    // console.log("grade id", e.target.value);
+    // const selSection =
+    //   gradeAndSection.filter((grade) => {
+    //     return grade.grade_level_id === e.target.value;
+    //   })[0].sections ?? [];
+    // setSectionList(selSection);
+  };
+
+  const handlerTableRow = (event, student) => {
+    // setSelectedStudent(student);
+    // setOpen(true);
+    // console.log(student);
+    // navigate(`/students/student?id=${student.std_id}`);
+  };
+
+  const handlePageClick = (e, value: number) => {
+    // navigate(`/students/all?page=${value}`);
+  };
+
+  return (
+    <Container onKeyDownCapture={handlePressEnter}>
+      <Title>All Teachers</Title>
+      <SearchContainer>
+        <InputName
+          placeholder="Search by Name"
+          onInput={handlerSearchName}
+        />
+
+        <Selection
+          placeholder="Select Class"
+          ref={gradeLevelRef}
+          onInput={handleClassName}
+        >
+          <SelectionItem value={""}> Select Class </SelectionItem>
+        </Selection>
+        <Selection ref={sectionRef}>
+          <SelectionItem value={""}> Select Section </SelectionItem>
+        </Selection>
+        <ButtonSearch onClick={handlerSearch}>Search</ButtonSearch>
+      </SearchContainer>
+      <SubjectsTable data={data} />
+      <PagerContainer>
+        <Pagination
+          count={pageCount}
+          page={currentPage}
+          onChange={handlePageClick}
+        />
+        {/* <TablePagination
+          component="div"
+          rowsPerPageOptions={[10]}
+          count={data.length}
+          rowsPerPage={rowPerPage}
+          page={page}
+          onPageChange={handleOnPageChange}
+          onRowsPerPageChange={handleOnRowsPerChange}
+          style={{ display: "flex", alignItems: "center" }}
+        /> */}
+      </PagerContainer>
+    </Container>
+  );
+};
+
+export default AllSubjects;
---
+++ src/pages/Subjects/Subjects.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import styled from "styled-components";
+import { Outlet } from "react-router-dom";
+
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 50px;
+  background-color: #F0F1F3;
+`;
+
+const PageHeader = styled.h1`
+  width: 100%;
+  margin-bottom: 20px;
+`;
+
+const LinkContainer = styled.ul`
+  width: 100%;
+  display: flex;
+  margin-bottom: 50px;
+  padding-left: 0px;
+
+`;
+
+const LinkItem = styled.li`
+  list-style-type: none;
+  margin: 0 10px 0 0;
+`;
+
+const NLink = styled.a``;
+
+const Subjects = () => {
+  return (
+    <>
+    <Container>
+      <PageHeader>Teacher</PageHeader>
+
+      <LinkContainer>
+        <LinkItem>
+          <NLink href="#">Home</NLink>
+        </LinkItem>
+        <LinkItem>
+        {'>'}
+        </LinkItem>
+        <LinkItem>
+          <NLink href="#">Teachers</NLink>
+        </LinkItem>
+      </LinkContainer>
+      <Outlet/>
+    </Container>
+    </>
+  )
+}
+
+export default Subjects
