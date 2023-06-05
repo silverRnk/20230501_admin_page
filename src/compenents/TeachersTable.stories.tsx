aa3b9d665cbff91404aaa5fe70418962fed50394
pdbautista0 <patdi.bautista@gmail.com>
2023-06-06 01:25:02 +0800

    Create TeachersTable Component
    Create Component
    Created Stories
    Edited ArrayGenerator

---
+++ src/compenents/TeachersTable.stories.tsx
@@ -0,0 +1,56 @@
+import { Meta, StoryObj } from "@storybook/react";
+import TeachersTable from "./TeachersTable";
+import { EmptyArrayGenerator } from "../utils/ArrayGenerator";
+import { faker } from "@faker-js/faker";
+import { TeacherProfileShort } from "../pages/Teachers/utils/interface";
+import Theme from "../Theme";
+
+const meta: Meta<typeof TeachersTable> = {
+  title: "Teachers Table",
+  component: TeachersTable,
+  decorators: [
+    (Story) => (
+        <Theme>
+            <Story/>
+        </Theme>
+    )
+  ]
+};
+
+export default meta;
+type Story = StoryObj<typeof TeachersTable>;
+
+export const Empty: Story = {
+  args: {
+    data: [],
+    isLoading: false,
+  },
+};
+
+const teachers: Array<TeacherProfileShort> = EmptyArrayGenerator(
+  5
+).map(() => ({
+  teacher_id: faker.string.alphanumeric({ length: 8 }),
+  teacher_name: faker.person.fullName(),
+  teacher_addr: faker.location.secondaryAddress(),
+  teacher_gender: faker.helpers.arrayElement(["Male", "Female"]),
+  teacher_class: faker.helpers.arrayElement(["", "class1", "class2"]),
+  teacher_dob: faker.date
+    .birthdate({ min: 20, max: 65 })
+    .toLocaleDateString(),
+  teacher_phone: faker.phone.number(),
+}));
+
+export const Loading: Story = {
+  args: {
+    data: [...teachers],
+    isLoading: true,
+  },
+};
+
+export const Filled: Story = {
+  args: {
+    data: [...teachers],
+    isLoading: false,
+  },
+};
---
+++ src/compenents/TeachersTable.tsx
@@ -0,0 +1,128 @@
+import React, { memo } from "react";
+import {
+  TableContainer,
+  Table,
+  TableHead,
+  TableRow,
+  Paper,
+  TableCell,
+  TableBody,
+} from "@mui/material";
+import { TeacherProfileShort } from "../pages/Teachers/utils/interface";
+import { columnTeachers } from "../pages/Teachers/utils/ColumnLabels";
+import { EmptyArrayGenerator } from "../utils/ArrayGenerator";
+import { PlaceHolder } from "./style-components/StyleComponents";
+import { relative } from "path";
+import styled from "styled-components";
+import { theme } from "../Theme";
+
+const EmptyLabelContainer = styled.div<{ isVisible: boolean }>`
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: ${(props) => (props.isVisible ? "flex" : "none")};
+  justify-content: center;
+  align-items: center;
+`;
+const EmptyLabel = styled.span`
+  font-size: ${(props) => props.theme.fontSize.medium1};
+`;
+
+const TeachersTableLoadingFiller = () => {
+  const column = columnTeachers;
+  const array = EmptyArrayGenerator(5);
+  return (
+    <>
+      {array.map(() => (
+        <TableRow hover style={{cursor:"pointer"}}>
+          {column.map(() => (
+            <TableCell>
+              <PlaceHolder style={{ height: "20px" }} />
+            </TableCell>
+          ))}
+        </TableRow>
+      ))}
+    </>
+  );
+};
+
+const TeachersTable = (args: {
+  data: Array<TeacherProfileShort>;
+  isLoading: boolean;
+  onSelectRow: (event, teacher: TeacherProfileShort) => void;
+}) => {
+  const column = columnTeachers;
+  const { data, isLoading, onSelectRow } = args;
+
+  return (
+    <TableContainer
+      component={Paper}
+      sx={{
+        border: "1px solid black",
+        width: "100%",
+        position: "relative",
+        minHeight: "200px",
+      }}
+    >
+      <Table size="medium">
+        <TableHead>
+          {column.map((col) => {
+            return (
+              <TableCell
+                key={col.id}
+                style={{
+                  minWidth: col.minWidth,
+                  textAlign: "center",
+                }}
+              >
+                {col.label}
+              </TableCell>
+            );
+          })}
+        </TableHead>
+        <TableBody>
+          {isLoading ? (
+            <TeachersTableLoadingFiller />
+          ) : (
+            data.map((teacher) => (
+              <TableRow
+                className="table-body-row"
+                hover
+                onClick={(event) => {
+                  onSelectRow(event, teacher);
+                }}
+                key={teacher.teacher_id}
+                style={{ cursor: "pointer" }}
+              >
+                {column.map((col) => {
+                  let value;
+                  if (col.id === "teacher_dob") {
+                    value = teacher[col.id];
+                  } else {
+                    value = teacher[col.id];
+                  }
+
+                  return (
+                    <TableCell
+                      size="medium"
+                      style={{ fontSize: theme.fontSize.medium }}
+                    >
+                      {value || "N/A"}
+                    </TableCell>
+                  );
+                })}
+              </TableRow>
+            ))
+          )}
+        </TableBody>
+      </Table>
+      <EmptyLabelContainer isVisible={data.length == 0 && !isLoading}>
+        <EmptyLabel>No data to display</EmptyLabel>
+      </EmptyLabelContainer>
+    </TableContainer>
+  );
+};
+
+export default memo(TeachersTable);
