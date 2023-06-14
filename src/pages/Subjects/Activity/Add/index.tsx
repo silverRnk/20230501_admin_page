628e0fccc47314b49bc277a3ac676cad52cca4b4
pdbautista0 <patdi.bautista@gmail.com>
2023-06-15 01:10:13 +0800

    Created Activity Pages Add ActivityPage Edit ActivityPage

---
+++ src/pages/Subjects/Activity/Add/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { PageContainer, PageTitle } from '../../../../compenents/style-components/PageStyleComponents'
+
+
+const AddActivity = () => {
+  return (
+    <PageContainer>
+        <PageTitle>Add Activity</PageTitle>
+    </PageContainer>
+  )
+}
+
+export default AddActivity
